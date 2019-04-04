32a7c043f489e6fe9e5a3c6acddec8c4db452ec8
nick <nmancus1@dtcc.edu>
2019-04-04 01:07:36 -0400

    went with xlsx instead of CSV

--- src/main/java/CSVFileReader.java
+++ src/main/java/CSVFileReader.java
@@ -1,39 +1,76 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+import java.util.*;
 
 public class CSVFileReader {
 
-    public static Map<String, Book> buildBookMap(String CSVFileName) {
+    public static Map<String, Book> buildBookMap(String XLSXFilename) throws IOException {
 
         Map<String, Book> bookMap = new HashMap<>();
 
-        try {
+        File excelFile = new File(XLSXFilename);
+        FileInputStream fis = new FileInputStream(excelFile);
 
-            BufferedReader br = new BufferedReader(new FileReader(CSVFileName));
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] bookData = line.split(",");
+        //Create an XSSF Workbook
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
 
-                Book newBook = new BookBuilder()
-                        //TODO:these fields must be modified to work with DTCC books
-                        .setTitle(bookData[0])
-                        .setAuthor(bookData[1])
-                        .setCourseNumber(bookData[2])
-                        .setNotes(bookData[3])
-                        .createBook();
+        //Get first sheet
+        XSSFSheet sheet = workbook.getSheetAt(0);
 
-                bookMap.put(newBook.title, newBook);
+        //Iterate on rows
+        Iterator<Row> rowIt = sheet.iterator();
+
+        //Skip first row, because headers
+        Row row = rowIt.next();
+
+
+        //Keep iterating through rows until there's no data left
+        while (rowIt.hasNext()) {
+            row = rowIt.next();
+
+            // iterate on cells for the current row
+            Iterator<Cell> cellIterator = row.cellIterator();
+
+            //Make new String[] for each row of toner info
+            String[] bookData = new String[15];
+
+            //Index to keep track of cells
+            int index = 0;
+
+            //Loop through the row, reading each cell into tonerInfoStringArray
+            while (cellIterator.hasNext()) {
+
+                //Get next cell
+                Cell cell = cellIterator.next();
+
+                //Extract data and assign to correct index of tonerInfoStringArray
+                bookData[index] = cell.toString();
+                index++;        //increment index
 
             }
 
-        } catch (IOException e) {
-            System.out.println("Problem parsing file!");
+            Book newBook = new BookBuilder()
+                    .setCourseNumber(bookData[1])
+                    .setSemesterCode(bookData[2])
+                    .setAuthor(bookData[4])
+                    .setTitle(bookData[5])
+                    .setIsbn(bookData[7])
+                    .setNotes(bookData[9])
+                    .createBook();
+
+            bookMap.put(newBook.title, newBook);
+
         }
 
-            return bookMap;
-    }
+        //Close workbook and fis
+        workbook.close();
+        fis.close();
 
+        //fin
+        return bookMap;
+    }
 }
