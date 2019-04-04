9f889b33547085803aaf10292203fa3571e6f3cf
nick <nmancus1@dtcc.edu>
2019-04-04 10:28:32 -0400

    add exception handling

--- src/test/java/CSVFileReaderTest.java
+++ src/test/java/CSVFileReaderTest.java
@@ -3,9 +3,12 @@ import java.util.Map;
 public class CSVFileReaderTest {
 
     public static void main(String[] args) {
-
-           Map<String, Book> bookmap =  CSVFileReader.buildBookMap("Book_Data/books.csv");
+        try {
+            Map<String, Book> bookmap = XLSXFileReader.buildBookMap("Book_Data/books.csv");
             System.out.println(bookmap);
+        } catch (Exception e) {
+            System.out.println("Whoops");
+        }
 
     }
 
